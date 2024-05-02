b4ae04d31582b418a89d2d9704b84bdb8bb4f0d0
Esteban Espinoza <estebandesarrolladordesoftware@gmail.com>
2024-05-02 10:41:56 -0500

    config

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ export const PORT = process.env.PORT || 7500;
 export const DB_HOST = process.env.DB_HOST || "localhost";
 export const DB_USER = process.env.DB_USER || "root";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "root123";
-export const DB_NAME = process.env.DB_NAME || "db_fedup_sistema_resultado";
+export const DB_NAME = process.env.DB_NAME || "db_fedup_sistema_resultados";
 export const DB_PORT = process.env.DB_PORT || 3306;
