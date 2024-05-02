8a0ae2948bd0242849e7c98bdbe122d1973751bb
Esteban Espinoza <estebandesarrolladordesoftware@gmail.com>
2024-05-02 13:14:20 -0500

    fsasasdsadsda

--- config/Database.js
+++ config/Database.js
@@ -3,9 +3,13 @@ import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } from "../config.js";
 
 // db_fedup_prueba_local
 // const db = new Sequelize("db_fedup_sistema_resultados", "root", "root123", {
-const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
+const db = new Sequelize({
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   dialect: "mysql",
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
 });
 
 export default db;
