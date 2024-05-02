1e9b73675bb7a0577b2aa8e7102ff9e2bdd18daa
Esteban Espinoza <estebandesarrolladordesoftware@gmail.com>
2024-05-02 12:54:32 -0500

    funciona por favor

--- config/Database.js
+++ config/Database.js
@@ -1,12 +1,15 @@
 import { Sequelize } from "sequelize";
-import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT } from "../config.js";
+import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } from "../config.js";
 
 // db_fedup_prueba_local
 // const db = new Sequelize("db_fedup_sistema_resultados", "root", "root123", {
-const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-  host: DB_HOST,
+const db = new Sequelize({
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   dialect: "mysql",
-  port: DB_PORT,
+  port: process.env.DB_PORT,
+  host: process.env.DB_HOST,
 });
 
 export default db;
