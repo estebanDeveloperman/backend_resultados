c75f7b0bb57e43f8e63666239d207fc6c663c25c
Esteban Espinoza <estebandesarrolladordesoftware@gmail.com>
2024-05-02 11:33:03 -0500

    aver p

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 export const PORT = process.env.PORT || 7500;
 
-export const DB_HOST = process.env.DB_HOST || "localhost";
-export const DB_USER = process.env.DB_USER || "root";
-export const DB_PASSWORD = process.env.DB_PASSWORD || "root123";
-export const DB_NAME = process.env.DB_NAME || "db_fedup_sistema_resultados";
-export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_HOST = process.env.MYSQLHOST || "localhost";
+export const DB_USER = process.env.MYSQLUSER || "root";
+export const DB_PASSWORD = process.env.MYSQLPASSWORD || "root123";
+export const DB_NAME =
+  process.env.MYSQLDATABASE || "db_fedup_sistema_resultados";
+export const DB_PORT = process.env.MYSQLPORT || 3306;
